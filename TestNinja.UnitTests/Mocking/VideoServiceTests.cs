34a7439f4e9c4bccc3be77488687a5a11076a3c3
CMiddleton <CMiddleton@CMIDDLETON-PC>
2018-03-05 15:29:36 -0600

    Interface for file system.

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Design;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+	[TestFixture]
+	internal class VideoServiceTests
+	{
+		[Test]
+		public void ReadVideoTitle_EmptyFile_ReturnError()
+		{
+			var service = new VideoService();
+			service.FileReader = new FakeFileReader();
+			var result = service.ReadVideoTitle();
+
+			Assert.That(result, Does.Contain("error").IgnoreCase);
+
+		}
+	}
+}
